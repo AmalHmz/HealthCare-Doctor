2feb09c641d1a1bac6feffa2d4456b8c60752c72
AmalHmz <121427490+AmalHmz@users.noreply.github.com>
2023-06-05 16:15:37 +0200

    new profile file
    some changes

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,46 +1,175 @@
 
+import 'package:firstly/utils/config.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 
 class ProfilePage extends StatelessWidget{
+ 
+  
   @override
-  Widget build (BuildContext context ) {
-    return  Scaffold(
-      backgroundColor: Colors.white, // Set the background color of the page
-      body: Container(
-        padding: EdgeInsets.all(16),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        SizedBox(height:30),
-        Text(
-          '  Profile',
-          style: TextStyle(
-            fontSize: 40,
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
-            decoration: TextDecoration.none,
-          ), 
-        ),
-         SizedBox(height: 30),
-          ListTile(
-            leading: CircleAvatar(radius: 40,
-                   backgroundImage: AssetImage("assets/images/doctor1.jpg"),
-                   
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Config.primaryColor,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height:80),
+            Padding(padding: EdgeInsets.symmetric(horizontal: 10),
+            child: Stack(
+              children: [
+                SizedBox(height: 100),
+               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  InkWell(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    
+                    child: Icon(CupertinoIcons.arrow_left,
+                    color: Colors.white,
+                    size: 25,
+                    ),
+                  ),
+                   InkWell(
+                    onTap: () {
+                    },
+                    
+                    child: Icon(CupertinoIcons.ellipsis,
+                    color: Colors.white,
+                    size: 25,
+                    ),
+                  ),
+                ],
+               ),
+               Padding(padding: EdgeInsets.symmetric(vertical: 10),
+               child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                CircleAvatar(radius: 35,
+                backgroundImage: AssetImage("assets/images/doctor1.jpg"),
                 ),
-           
-            title: Text("Mohamed Hemaizia",
-            style: TextStyle(
-              fontWeight: FontWeight.w500,
-              fontSize: 25, 
+                SizedBox(height: 15),
+                Text("Dr Doctor Name",
+                style: TextStyle(
+                  fontSize: 23,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white,
+                ),),
+                SizedBox(height: 5),
+                Text("Speciality ",
+                style: TextStyle(
+                   color: Colors.white,
+                   fontWeight: FontWeight.bold,
+
+                ),),
+                SizedBox(height: 5),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    
+                  ],
+                ),
+               ],),
+               ),
+              ],
             ),
             ),
-            subtitle: Text("He/him  "),
-          ),
-           
-      ],
-    ),
+            SizedBox(height: 20),
+            Container(
+              height: MediaQuery.of(context).size.height / 1.5 ,
+              width: double.infinity,
+              padding: EdgeInsets.only(
+                top: 20,
+                left: 15,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  topRight: Radius.circular(10),
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max ,
+                children: [
+                  Text("BIO",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                  ),),
+                  SizedBox(height: 5),
+                  Text("Dr Doctor Name is a (Speciality) doctor , he have an experience of % years ...",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black54,
+                      
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Text("Reviews",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                      ),),
+                      SizedBox(width: 10),
+                      Icon(CupertinoIcons.star_fill, color: Colors.amber),
+                      SizedBox(width: 10),
+                      Text("4.9",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),),
+                      SizedBox(width: 5),
+                      Text("(130 review)",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        color: Config.primaryColor,
+                      ),),
+                   
+                    ],
+                  ),
+                 
+                  
+                  SizedBox(height: 20),
+                  Text("Location",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                  ),),
+                  ListTile(
+                    leading: Container(
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        
+                      ),
+                      child: Icon(CupertinoIcons.pin,
+                      color: Config.primaryColor,
+                      size: 30,),
+                    ),
+                    title: Text("Manaa, Mohamed Benaamri Clinic",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+
+                      ),
+                    ),
+                    subtitle: Text("adress line of the clinic ,"),
+                  ),
+                ],
+              ),
+              
+            ),
+
+          ],
+        ),
       ),
-  );
-}
+     
+    );
+  }
 }
